73b0558531dcf081f2cfb4ccde9045b5b094e5a2
Ramazan USLU <ruslu1720@gmail.com>
2022-07-29 01:47:03 +0300

    Dropdown menu icin yazilan baslangic kodlarında iyilestirme & Diger kod iyilestirme

--- js/index.js
+++ js/index.js
@@ -13,6 +13,7 @@ document
 document.getElementById("btn-close").addEventListener("click", function () {
   objOffcanvas.hide();
 });
+//-----------------------------------------------------------------------------
 //Mobil ekranlarda search işlemi için kullanılacak olan modal kutusunun kontrolü
 const modalDOM = document.getElementById("exampleModal");
 const objModal = new bootstrap.Modal(modalDOM, {
@@ -28,19 +29,43 @@ document
   .addEventListener("click", function () {
     objModal.hide();
   });
+//-----------------------------------------------------------------------------
 //Dropdown için gerekli olan javascript
-function DropdownControl() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
-window.onclick = function (event) {
-  if (!event.target.matches(".dropbtn")) {
-    let dropdowns = document.getElementsByClassName("dropdown-content");
-    let i;
-    for (i = 0; i < dropdowns.length; i++) {
-      let openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains("show")) {
-        openDropdown.classList.remove("show");
-      }
-    }
-  }
-};
+//-----------------------------------------------------------------------------
+//function DropdownControl() {
+// document.getElementById("myDropdown").classList.toggle("show");
+// const TvSes = [
+//   "Televizyonlar",
+//   "Kulaklıklar",
+//   "Hoparlörler",
+//   "Ev Sinema Sistemleri",
+// ];
+// const ul = document.getElementById("dropdown-ul");
+// for (let index = 0; index < TvSes.length; index++) {
+//   console.log(TvSes[index]);
+//   ul.innerHTML =
+//     "<ul id='dropdown-ul' class='nav flex-column'><li class='nav-item'><a class='nav-link dropdown-list' href='/'><i class='list-item fa-solid fa-chevron-right'></i>" +
+//     TvSes[index] +
+//     "</a></li></ul>";
+// }
+//}
+// window.onclick = function (event) {
+//   if (!event.target.matches(".dropbtn")) {
+//     let dropdowns = document.getElementsByClassName("dropdown-content");
+//     let i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       let openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains("show")) {
+//         openDropdown.classList.remove("show");
+//       }
+//     }
+//   }
+// };
+//-------------------------------------------------------------------------------
+document
+  .getElementById("tv-ses-btn")
+  .addEventListener("mouseover", function () {
+    console.log("Tv - Ses Butonu üzerine hover olundu");
+    document.getElementById("myDropdown").classList.toggle("show");
+    document.getElementById("tv-ses").classList.toggle("active");
+  });
